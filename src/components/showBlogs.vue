94e2b49f67c45e50978d1a9df2e014be631aeec5
codwerk <louis_widdy@live.fr>
2020-06-21 13:24:47 +0200

    request get API and integration html/css

--- src/components/showBlogs.vue
+++ src/components/showBlogs.vue
@@ -1,15 +1,44 @@
 <template>
-  <div></div>
+  <div id="showBlogs">
+    <h1>All articles</h1>
+    <div v-for="(post, index) in posts" :key="index" class="single-blog">
+      <h2>{{ post.title}}</h2>
+      <p>{{ post.body }}</p>
+
+      <hr v-if="posts.length-1">
+    </div>
+  </div>
 </template>
 
 <script>
  export default {
    data: () => {
-     return {}
+     return {
+       posts: []
+     }
+   },
+   mounted(){
+     this.$http.get('https://jsonplaceholder.typicode.com/posts').then((data) => {
+       this.posts = data.body.slice(0,10);
+     })
    }
   }
 </script>
 
 <style lang="scss" scoped>
-  
+  #showBlogs{
+    max-width: 800px;
+    margin: 0 auto;
+    h1 {
+        text-align: center;
+      }
+    .single-blog{
+
+      
+
+      h2, p {
+        text-transform: lowercase;
+      }
+    }
+  }
 </style>
